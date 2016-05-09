307b4b2b85d748703e90fd917816a5d283dac918
KellerJordan <kellerjordan1998@gmail.com>
2016-05-09 00:20:38 -0700

    Added picking algorithm

--- winrateWriter.php
+++ winrateWriter.php
@@ -2,18 +2,41 @@
 
 $con=mysqli_connect("localhost","root","","herodb");
 $data=json_decode($_POST["data"]);
-$name=$_POST["name"];
 
-if($_POST["type"]=="out"){
+switch($_POST["type"]){
+case "out":
+	$name=$_POST["name"];
 	mysqli_query($con,"DROP TABLE `$name`");
 	mysqli_query($con,"CREATE TABLE `$name` (
-    `name` varchar(255) NOT NULL default '',
-    `advantage` float(11) NOT NULL default 0
+	`name` varchar(255) NOT NULL default '',
+	`advantage` float(11) NOT NULL default 0
 	)");
 	for($i=0;$i<count($data);$i++){
 		$heroName=$data[$i]->name;
 		$heroAdvantage=$data[$i]->advantage;
 		mysqli_query($con,"INSERT INTO `$name` (name,advantage) VALUES ('$heroName',$heroAdvantage)");
 	}
+break;
+case "in":
+	$rows=[];
+	for($i=0;$i<count($data);$i++){
+		$name=$data[$i];
+		$result=$con->query("SELECT*FROM `$name`");
+		while($row=$result->fetch_assoc()){
+			$hName=$row["name"];
+			$hAdvantage=$row["advantage"];
+			if(!(in_array($hName,$data))){
+				if(array_key_exists($hName,$rows)){
+					$rows[$hName]=bcadd($rows[$hName],$hAdvantage,4);
+				}else{
+					$rows[$hName]=$hAdvantage;
+				}
+			}
+		}
+	}
+	asort($rows);
+	echo json_encode($rows);
+break;
 }
+
 ?>
